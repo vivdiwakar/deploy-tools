9d1230fc627164e296f02b4dfb36cef68adc2a41
Viv Diwakar <viv@vdiwakar.com>
2017-04-06 00:54:25 +0100

    Updated Go installer for correct paths, updating PATH as well

--- golang/install_golang.pp
+++ golang/install_golang.pp
@@ -5,22 +5,31 @@
 # Installer for downloading updtream Go, and installing
 #
 
+include stdlib
+
 $paths = [
   '/bin',
   '/usr/bin',
   '/usr/local/bin',
 ]
 
-exec { 'update_go_paths':
-  require => Exec[
-    'deploy_go_source'
+exec { 'update_exec_path':
+  require => File_Line[
+    'add_goroot_path'
   ],
   cwd => "/home/$::guser",
   path => $paths,
-  command => "echo $::guser > gopher.txt",
-  creates => "/home/$::guser/gopher.txt",
+  command => "sed -e 's/\(.* PATH=.*\)/\1:\/usr\/local\/go\/bin/g' -i.bak.`date +%Y%m%d-%s` /home/$::guser/.bashrc",
   returns => [0],
   logoutput => on_failure,
+}
+
+file_line { 'add_goroot_path':
+  require => Exec[
+    'deploy_go_source'
+  ],
+  path => "/home/$::guser/.bashrc",
+  line => 'export GOROOT=/usr/local/go',
 }
 
 exec { 'deploy_go_source':
