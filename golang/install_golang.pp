52dc4f6b6a0e4f982d0f914792d82a1d75a67420
vivd <viv@ggh.uk>
2017-05-03 12:55:44 +0100

    Fixed formatting

--- golang/install_golang.pp
+++ golang/install_golang.pp
@@ -1,9 +1,7 @@
-######
-######
 #
 # Author: 	Viv Diwakar <viv@vdiwakar.com>
 # Date:   	20170406
-# Last update:	20170503 12:54:12 BST (+0100)
+# Last update:	20170503 12:55:44 BST (+0100)
 #
 # Installer for downloading upstream Go, and installing
 #
